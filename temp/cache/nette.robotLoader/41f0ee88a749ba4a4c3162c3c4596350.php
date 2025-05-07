df6778fe152d38deb498c0faee9d1819100421d5
Vladislav Rajtmajer <rajtik@gmail.com>
2025-05-07 10:22:29 +0200

    Initial commit

---
+++ temp/cache/nette.robotLoader/41f0ee88a749ba4a4c3162c3c4596350.php
@@ -0,0 +1,56 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Core/RouterFactory.php',
+      1 => 1738539948,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Bootstrap.php',
+      1 => 1738539948,
+    ),
+    'App\\Http\\Middleware\\DenyMiddleware' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Http/Middleware/DenyMiddleware.php',
+      1 => 1746605998,
+    ),
+    'App\\Presentation\\Home\\HomePresenter' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Home/HomePresenter.php',
+      1 => 1738539948,
+    ),
+    'App\\Presentation\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Accessory/LatteExtension.php',
+      1 => 1738539948,
+    ),
+    'App\\Presentation\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1738539948,
+    ),
+    'App\\Presentation\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1738539948,
+    ),
+    'App\\Interfaces\\MiddlewareInterface' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Interfaces/MiddlewareInterface.php',
+      1 => 1746605932,
+    ),
+  ),
+  1 => 
+  array (
+    'Nette\\Bridges\\Assets\\DIExtension' => 3,
+    'null' => 3,
+    'App\\Presentation\\Home\\HomeDefaultTemplate' => 3,
+    'App\\Presentation\\Home\\HomeTemplate' => 3,
+  ),
+  2 => 
+  array (
+  ),
+);
---
+++ temp/cache/nette.search/41f0ee88a749ba4a4c3162c3c4596350.php
@@ -0,0 +1,52 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Core/RouterFactory.php',
+      1 => 1738539948,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Bootstrap.php',
+      1 => 1738539948,
+    ),
+    'App\\Http\\Middleware\\DenyMiddleware' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Http/Middleware/DenyMiddleware.php',
+      1 => 1746605998,
+    ),
+    'App\\Presentation\\Home\\HomePresenter' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Home/HomePresenter.php',
+      1 => 1738539948,
+    ),
+    'App\\Presentation\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Accessory/LatteExtension.php',
+      1 => 1738539948,
+    ),
+    'App\\Presentation\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1738539948,
+    ),
+    'App\\Presentation\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Presentation/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1738539948,
+    ),
+    'App\\Interfaces\\MiddlewareInterface' => 
+    array (
+      0 => '/Users/rajtik/PhpstormProjects/NetteProjects/nette-route-middleware/app/Interfaces/MiddlewareInterface.php',
+      1 => 1746605932,
+    ),
+  ),
+  1 => 
+  array (
+  ),
+  2 => 
+  array (
+  ),
+);
